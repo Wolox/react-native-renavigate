4c8e0214e3637ba843ed7622763c360efed04f67
Sebastian Balay <snbalay@gmail.com>
2017-01-16 19:15:02 -0300

    add build hook to publish package

---
+++ index.js
@@ -0,0 +1,10 @@
+import reducer from './src/reducer';
+import { actionCreators } from './src/actions';
+import RootSceneContainer from './src/RootSceneContainer';
+
+
+export default {
+  reducer,
+  actionCreators,
+  RootSceneContainer
+};
