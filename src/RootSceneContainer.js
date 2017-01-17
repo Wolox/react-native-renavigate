97ac1e5b9dfed5a6f6b6eff3dd3a3cca54a81cfc
Sebastian Balay <snbalay@gmail.com>
2017-01-17 14:35:37 -0300

    comunicate current route using context

--- src/RootSceneContainer.js
+++ src/RootSceneContainer.js
@@ -16,15 +16,31 @@ class RootSceneContainer extends Component {
     initActions(props.routes); // TODO: esto va a pisar el resto? no permitir rutas con el mismo nombre!
   }
 
+  getChildContext() {
+    return { activeRouteInstance: this.getCurrentRoute() || this.props.initialRoute };
+  }
+
   componentWillReceiveProps({ activeRoute, navigationMethod }) {
     if (this.props.activeRoute !== activeRoute) {
-      const navigator = this.refs[RootSceneContainer.refs.navigatorComponent];
+      const navigator = this.getNavigator();
       if (navigationMethod === actions.POP || navigationMethod === actions.POP_TO_TOP) {
         navigator[navigationMethod]();
       } else if (navigationMethod === actions.PUSH || navigationMethod === actions.RESET_TO) {
         navigator[navigationMethod](this.props.routes[activeRoute.name](activeRoute.params));
       }
     }
+  }
+
+  getNavigator() {
+    return this.refs[RootSceneContainer.refs.navigatorComponent];
+  }
+
+  getCurrentRoute() {
+    const navigator = this.getNavigator();
+    if (!navigator || !navigator.navigationContext) {
+      return null;
+    }
+    return navigator.navigationContext.currentRoute;
   }
 
   renderScene = (route) => {
@@ -51,7 +67,7 @@ class RootSceneContainer extends Component {
 
 // TODO: be more specific. !important
 RootSceneContainer.propTypes = {
-  activeRoute: React.PropTypes.any.isRequired,
+  activeRoute: React.PropTypes.any,
   decorateRouteComponent: React.PropTypes.func.isRequired,
   defaultTransition: React.PropTypes.any.isRequired,
   initialRoute: React.PropTypes.any.isRequired,
@@ -65,6 +81,10 @@ RootSceneContainer.defaultProps = {
   navigationBar: (dispatch) => {
     return <Navigator.NavigationBar routeMapper={navigationBarRouteMapper(dispatch)} />;
   }
+};
+
+RootSceneContainer.childContextTypes = {
+  activeRouteInstance: React.PropTypes.any.isRequired
 };
 
 const mapStateToProps = (store) => {
