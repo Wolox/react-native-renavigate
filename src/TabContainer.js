6a0216035d88e34f720bc93efa2c8ce17f9017b5
Sebastian Balay <snbalay@gmail.com>
2017-01-17 17:20:39 -0300

    validate tab container props presence

--- src/TabContainer.js
+++ src/TabContainer.js
@@ -3,6 +3,9 @@ import { connect } from 'react-redux';
 import RootScene from './RootScene';
 
 const mapStateToProps = (store, props) => {
+  if (!props.tabIndex && props.tabIndex !== 0) {
+    throw new Error('prop "tabIndex" must be present in TabContainer');
+  }
   const navState = store.navigation;
   return {
     activeRoute: navState[props.tabIndex] && navState[props.tabIndex].activeRoute,
